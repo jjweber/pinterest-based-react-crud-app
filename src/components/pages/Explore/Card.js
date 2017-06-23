a0db31edb230fc558e5345d0ae3b063084ce0ee0
Daniel Weber <dw@thedanielweber.com>
2017-06-23 15:37:03 -0400

    Editing pins and boards now working

--- src/components/pages/Explore/Card.js
+++ src/components/pages/Explore/Card.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Button } from 'reactstrap';
+
 class Card extends Component {
   constructor(props) {
       super(props);
@@ -8,6 +10,7 @@ class Card extends Component {
         board: this.props.item
       };
 
+      this.editBoard = this.editBoard.bind(this);
       this.deleteBoard = this.deleteBoard.bind(this);
       this.passUpItemToShow = this.passUpItemToShow.bind(this);
   }
@@ -16,9 +19,13 @@ class Card extends Component {
     this.props.showItem(this.state.board);
   }
 
+  editBoard() {
+    console.log("Will pass up request to edit board with id of: ", this.state.board.id);
+    this.props.editItem(this.state.board.id);
+  }
+
   deleteBoard() {
     console.log("Will pass up request to delete board with id of: ", this.state.board.id);
-
     //this.props.removeItem(this.state.board.id);
   }
 
@@ -33,10 +40,17 @@ class Card extends Component {
       <div className="cards_item">
         <div className="card">
           <div className="card_content">
-            <div onClick={this.deleteBoard} className="pin-delete"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
+            <div className="pull-right">
+              <div onClick={this.deleteBoard} className="pin-delete"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
+              <div onClick={this.editBoard} className="pin-edit"><span className="glyphicon glyphicon-pencil" aria-hidden="true"></span></div>
+            </div>
+
             <div className="card_title">{this.state.board.name}</div>
-            <div onClick={this.passUpItemToShow} className="card_pins">
+            <div className="card_pins">
                 {boardPins}
+            </div>
+            <div className="card_footer">
+              <Button color="primary" size="lg" block onClick={this.passUpItemToShow}>Open Board</Button>
             </div>
           </div>
         </div>
--- src/components/pages/Explore/explorePage.js
+++ src/components/pages/Explore/explorePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Card from './Card';
 import Modal from '../modal';
 
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col, Button, InputGroup, Input } from 'reactstrap';
 
 class Explorepage extends Component {
   constructor(props) {
@@ -24,12 +24,86 @@ class Explorepage extends Component {
       this.state = {
         existingBoards: boardData,
         isModalOpen: false,
+        isCreateModalOpen: false,
+        isEditModalOpen: false,
+        isEditPinModalOpen: false,
+        newBoardName: "",
+        editedBoardName: "",
+        editedPinDescription: "",
         currentBoardToShow: initialBoardToShow,
-        boardDetailsPins: {}
+        boardDetailsPins: {},
+        currentBoardBeingEditing: {},
+        currentPinBeingEditing: {}
       };
 
       this.showBoardDetails = this.showBoardDetails.bind(this);
       this.deleteBoard = this.deleteBoard.bind(this);
+      this.editBoard = this.editBoard.bind(this);
+
+      this.openEditPinModal = this.openEditPinModal.bind(this);
+      this.closeEditPinModal = this.closeEditPinModal.bind(this);
+
+      this.openEditModal = this.openEditModal.bind(this);
+      this.closeEditModal = this.closeEditModal.bind(this);
+      this.openCreateModal = this.openCreateModal.bind(this);
+      this.closeCreateModal = this.closeCreateModal.bind(this);
+
+      this.updateNewBoardName = this.updateNewBoardName.bind(this);
+      this.updateEditedBoardName = this.updateEditedBoardName.bind(this);
+      this.updateEditedPinDescription = this.updateEditedPinDescription.bind(this);
+
+      this.saveNewBoard = this.saveNewBoard.bind(this);
+      this.saveEditedBoardName = this.saveEditedBoardName.bind(this);
+      this.saveEditedPinDescription = this.saveEditedPinDescription.bind(this);
+  }
+
+  updateNewBoardName(e) {
+    this.setState({ newBoardName: e.target.value });
+  }
+
+  updateEditedBoardName(e) {
+    this.setState({ editedBoardName: e.target.value });
+  }
+
+  updateEditedPinDescription(e) {
+    this.setState({ editedPinDescription: e.target.value });
+  }
+
+  saveNewBoard() {
+    // add to existingBoards and update the storage
+    let newBoard = {};
+    newBoard.id = this.state.existingBoards.length;
+    newBoard.name = this.state.newBoardName;
+    newBoard.pins = [];
+
+    this.state.existingBoards.push(newBoard);
+    this.updateBoardsInStorage();
+
+    this.closeCreateModal();
+  }
+
+  openEditPinModal() {
+    this.setState({ isEditPinModalOpen: true });
+  }
+
+  closeEditPinModal() {
+    this.setState({ isEditPinModalOpen: false });
+  }
+
+  openEditModal() {
+    this.setState({ isEditModalOpen: true });
+  }
+
+  closeEditModal() {
+    this.setState({ isEditModalOpen: false });
+  }
+
+  openCreateModal() {
+    this.setState({ isCreateModalOpen: true });
+  }
+
+  closeCreateModal() {
+    this.setState({ isCreateModalOpen: false });
   }
 
   openModal() {
@@ -40,6 +114,59 @@ class Explorepage extends Component {
       this.setState({ isModalOpen: false });
   }
 
+  editBoard(boardId) {
+    let currentBoardToEdit = this.state.existingBoards[boardId];
+    this.setState({editedBoardName: currentBoardToEdit.name});
+    this.setState({currentBoardBeingEditing: currentBoardToEdit});
+    this.openEditModal();
+  }
+
+  saveEditedPinDescription() {
+    let currentPinBeingEdited = this.state.currentPinBeingEditing;
+    currentPinBeingEdited.description = this.state.editedPinDescription;
+
+    let indexOfPinToEdit = this.getIndexOfPinById(currentPinBeingEdited.id);
+    let tempBoard = this.state.currentBoardToShow;
+    let tempPins = tempBoard.pins;//[indexOfPinToEdit];
+    tempPins[indexOfPinToEdit] = currentPinBeingEdited;
+
+    tempBoard.pins = tempPins;
+    this.setState({currentBoardToShow: tempBoard});
+
+    let tempBoards = this.state.existingBoards;
+    let boardIndexToChange = this.getBoardIndexByName(currentPinBeingEdited.name);
+    tempBoards[boardIndexToChange] = tempBoard;
+
+    this.setState({existingBoards: tempBoards});
+    this.updateBoardsInStorage();
+    this.closeEditPinModal();
+  }
+
+  getBoardIndexByName(boardName) {
+    // search each existingBoard for this name, return the index
+    let existingBoards = this.state.existingBoards;
+
+    for(let i = 0; i < existingBoards.length; i++) {
+      let currentCheck = existingBoards[i];
+      if(currentCheck.name === boardName) return currentCheck.id;
+    }
+
+    return -1;
+  }
+
+  saveEditedBoardName() {
+    let currentBoardBeingEdited = this.state.currentBoardBeingEditing;
+    currentBoardBeingEdited.name = this.state.editedBoardName;
+
+    let tempBoards = this.state.existingBoards;
+    tempBoards[currentBoardBeingEdited.id] = currentBoardBeingEdited;
+
+    this.setState({existingBoards: tempBoards});
+    this.updateBoardsInStorage();
+
+    this.closeEditModal();
+  }
+
   deleteBoard(boardId) {
     console.log("Will delete board with id of: ", boardId);
     let tempBoards = this.state.existingBoards;
@@ -48,6 +175,27 @@ class Explorepage extends Component {
     console.log("Exisitng boards after removing: ", tempBoards);
     //this.setState({existingBoards: tempBoards});
     //this.updateBoardsInStorage();
+  }
+
+  getIndexOfPinById(pinId) {
+    let currentBoardToCheck = this.state.currentBoardToShow;
+    for (let i = 0; i < currentBoardToCheck.pins.length; i++) {
+      let pinToCheck = currentBoardToCheck.pins[i];
+      if(pinToCheck.id === pinId) return i;
+    }
+    return -1;
+  }
+
+  editPin(pinId) {
+    let indexOfPinToEdit = this.getIndexOfPinById(pinId);
+    let pinToEdit = this.state.currentBoardToShow.pins[indexOfPinToEdit];
+
+    console.log("Pin Found to edit: ", pinToEdit);
+
+    this.setState({currentPinBeingEditing: pinToEdit});
+    this.setState({editedPinDescription: pinToEdit.description});
+
+    this.openEditPinModal();
   }
 
   deletePin(pinId) {
@@ -68,10 +216,7 @@ class Explorepage extends Component {
   }
 
   showBoardDetails(board) {
-    console.log("Showing board details for board: ", board);
-
     this.setState({currentBoardToShow: board});
-
     this.openModal();
   }
 
@@ -79,14 +224,17 @@ class Explorepage extends Component {
 
     let boardCards = this.state.existingBoards
         .map( board => {
-            return <Card item={board} removeItem={this.deleteBoard} showItem={this.showBoardDetails} key={board.id} />
+            return <Card item={board} editItem={this.editBoard} removeItem={this.deleteBoard} showItem={this.showBoardDetails} key={board.id} />
         });
 
 
     let currentBoardPinImages = this.state.currentBoardToShow.pins
         .map( pin => {
             return <div key={pin.id} className="pin-photo">
-              <div onClick={() => this.deletePin(pin.id)} className="pin-delete"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
+              <div className="pull-right pin-buttons">
+                <div onClick={() => this.editPin(pin.id)} className="pin-edit"><span className="glyphicon glyphicon-pencil" aria-hidden="true"></span></div>
+                <div onClick={() => this.deletePin(pin.id)} className="pin-delete"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></div>
+              </div>
               <img src={pin.imageUrl} />
               <h2 className="pin-name">{pin.name}</h2>
               <div className="pin-description">{pin.description}</div>
@@ -100,6 +248,16 @@ class Explorepage extends Component {
           Your Boards
         </h1>
         <div className="cards">
+          <div className="cards_item">
+            <div onClick={this.openCreateModal} className="card create-board-card">
+              <div className="plus-symbol-holder">
+                <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+              </div>
+              <div className="symbol-text">
+                Create New Board
+              </div>
+            </div>
+          </div>
           {boardCards}
         </div>
 
@@ -110,16 +268,73 @@ class Explorepage extends Component {
                 {currentBoardPinImages}
               </div>
             </div>
-            <Container>
-              <Row>
-                <Col>
-                  <Button color="secondary" size="lg" block onClick={() => this.closeModal()}>Close</Button>
-                </Col>
-              </Row>
-          </Container>
+            <div className="modal-footer">
+              <Button color="primary" size="lg" block onClick={() => this.closeModal()}>Close</Button>
+            </div>
+        </Modal>
+
+        <Modal assignedClass="normal" isOpen={this.state.isCreateModalOpen} onClose={() => this.closeCreateModal()}>
+            <div className="modal-header"><h1>Create New Board</h1></div>
+            <div className="modal-content">
+              <div className="cards current-board-details">
+                <InputGroup>
+                  <Input id="newBoardName" type="text" onChange={this.updateNewBoardName} placeholder="New Board Name" value={this.state.newBoardName} />
+                </InputGroup>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <Button onClick={this.saveNewBoard} color="primary" size="lg" block>Save</Button>
+            </div>
+        </Modal>
+
+        <Modal assignedClass="normal" isOpen={this.state.isEditModalOpen} onClose={() => this.closeEditModal()}>
+            <div className="modal-header"><h1>Editing {this.state.currentBoardBeingEditing.name}</h1></div>
+            <div className="modal-content">
+              <div className="cards current-board-details">
+                <InputGroup>
+                  <Input id="editedBoardName" type="text" onChange={this.updateEditedBoardName} value={this.state.editedBoardName} />
+                </InputGroup>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <Container>
+                <Row id="boardModalBtns">
+                  <Col>
+                    <Button onClick={this.saveEditedBoardName} color="primary" size="lg" block>Save</Button>
+                  </Col>
+                  <Col>
+                    <Button id="edit-cancel" color="danger" size="lg" block onClick={() => this.closeEditModal()}>Cancel</Button>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
         </Modal>
 
 
+
+
+        <Modal assignedClass="normal" isOpen={this.state.isEditPinModalOpen} onClose={() => this.closeEditPinModal()}>
+            <div className="modal-header"><h1>Editing {this.state.currentPinBeingEditing.name}</h1></div>
+            <div className="modal-content">
+              <div className="cards current-pin-details">
+                <InputGroup>
+                  <Input id="editedPinDescription" type="textarea" onChange={this.updateEditedPinDescription} value={this.state.editedPinDescription} />
+                </InputGroup>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <Container>
+                <Row id="boardModalBtns">
+                  <Col>
+                    <Button onClick={this.saveEditedPinDescription} color="primary" size="lg" block>Save</Button>
+                  </Col>
+                  <Col>
+                    <Button id="edit-cancel" color="danger" size="lg" block onClick={() => this.closeEditPinModal()}>Cancel</Button>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+        </Modal>
 
       </div>
     );
